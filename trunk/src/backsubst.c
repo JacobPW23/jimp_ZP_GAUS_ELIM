cb7832281cbcad3a54f9653b2860634e71243884
Michał Zalewski <michal.zalewski11.stud@pw.edu.pl>
2023-12-17 22:57:51 +0100

    Dodaje implementację podstawiania wstecznego

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "backsubst.h"
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
@@ -5,18 +6,36 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	if((mat->r)!=(mat->c))
+	{
+		printf("Blad. Nieprawidlowy rozmiar macierzy.\n");
+		return 2;
+	}
+	else
+	{
+	int ir, ic;
+	double suma=0.0;
+	int n=mat->r;
+	
+	for(ir=n-1;ir>=0;ir--)
+	{
+		suma=0;
+		for(ic=ir+1;ic<n;ic++)
+		{	
+			if(mat->data[ir][ic]==0)
+			{
+				printf("Blad. Dzielenie przez zero.\n");
+				return 1;
+			}
+			else
+				suma=suma + mat->data[ir][ic] * x->data[ic][0];		
+		}
+		
+		x->data[ir][0] = (b->data[ir][0] - suma)/mat->data[ir][ir];
+	}
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+	}
+	return 0;
 }
 
 
