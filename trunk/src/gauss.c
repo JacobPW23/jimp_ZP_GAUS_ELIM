1c5eb8fe0d14ab895594290626306a3bbb965792
Jakub Próchnicki <jakub.prochnicki.stud@pw.edu.pl>
2023-12-19 22:44:34 +0100

    Zaimplementowanie eliminacji gaussa z wyborem elementu diagonalnego

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -8,10 +8,26 @@ int eliminate(Matrix *mat, Matrix *b){
 
 int w=mat->r;
 int k=mat->c;
+if(w!=k)
+	return -1;
 double **dane=mat->data;
 double **rhsv=b->data;
+double* tmp;
 for(int i=0;i<k-1;i++)
 {
+	double max=dane[i][i];
+	for(int j=i+1;j<w;j++)
+	{
+	if(dane[j][i]>max)
+	{
+		tmp=dane[i];	
+		dane[i]=dane[j];
+		dane[j]=tmp;
+		tmp=rhsv[i];
+		rhsv[i]=rhsv[j];
+		rhsv[j]=tmp;
+	}
+	}
 	double a;
 	for(int j=i+1;j<w;j++)
 	{       if(dane[i][i])
