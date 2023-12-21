20b5e4ffc03064aa53d6682601973bde931b8749
Michał Zalewski <michal.zalewski11.stud@pw.edu.pl>
2023-12-21 17:45:52 +0100

    Poprawki błędów

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -11,30 +11,30 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 		printf("Blad. Nieprawidlowy rozmiar macierzy.\n");
 		return 2;
 	}
-	else
-	{
-	int ir, ic;
-	double suma=0.0;
-	int n=mat->r;
+	else {
+		int ir, ic;
+		double suma=0.0;
+		int n=mat->r;
 	
-	for(ir=n-1;ir>=0;ir--)
-	{
-		suma=0;
-		for(ic=ir+1;ic<n;ic++)
-		{	
-			if(mat->data[ir][ic]==0)
-			{
-				printf("Blad. Dzielenie przez zero.\n");
-				return 1;
-			}
-			else
+		for(ir=n-1;ir>=0;ir--)
+		{
+			suma=0;
+			for(ic=ir+1;ic<n;ic++)
+			{	
 				suma=suma + mat->data[ir][ic] * x->data[ic][0];		
-		}
+			}
 		
-		x->data[ir][0] = (b->data[ir][0] - suma)/mat->data[ir][ir];
-	}
+			if(mat->data[ir][ir]==0)
+                	{
+                        	printf("Blad. Dzielenie przez zero.\n");
+                        	return 1;
+                	}
+
+			x->data[ir][0] = (b->data[ir][0] - suma)/mat->data[ir][ir];
+		}
 
 	}
+
 	return 0;
 }
 
