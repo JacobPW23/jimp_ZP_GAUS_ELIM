e4f808cee9cf5f1879b2d75a395890772268aa01
Michał Zalewski <michal.zalewski11.stud@pw.edu.pl>
2023-12-21 17:26:51 +0100

    Dodaje moją implementacje selekcji elementu głównego

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -1,29 +1,45 @@
 #include "gauss.h"
-
+#include <math.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
 
-int w=mat->r;
-int k=mat->c;
-double **dane=mat->data;
-double **rhsv=b->data;
-for(int i=0;i<k-1;i++)
+int rows=mat->r;
+int cols=mat->c;
+double **data=mat->data;
+double **b_data=b->data;
+
+for(int i=0;i<cols-1;i++)
 {
-	double a;
-	for(int j=i+1;j<w;j++)
-	{       if(dane[i][i])
-		 	a=(dane[j][i]/dane[i][i]);
-		else return 1;
-		for(int z=i;z<k;z++)
-			dane[j][z]-=a*dane[i][z];
-		rhsv[j][0]-=a*rhsv[i][0];
+	int maxElem=i;
+	for(int k=i+1;k<rows;k++)
+	{
+		if(fabs(data[k][i])>fabs(data[maxElem][i]))
+			maxElem=k;
+	}
+	
+	if(i!=maxElem)
+	{
+		double *tmp=data[i];
+		data[i]=data[maxElem];
+		data[maxElem]=tmp;
+
+		double *tmpB=b_data[i];
+		b_data[i]=b_data[maxElem];
+		b_data[maxElem]=tmpB;
 	}
 
+	for(int j=i+1;j<rows;j++)
+	{       if(data[i][i]==0)
+		 	return 1;
+		double n = data[j][i] / data[i][i];
+		for(int z=i;z<cols;z++)
+			data[j][z]-=n*data[i][z];
+		b_data[j][0]-=n*b_data[i][0];
+	}
 }
-
 		return 0;
 }
 
